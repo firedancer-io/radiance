6d0ce088efda11d87a22aab08997eedcc8252f6a
Richard Patel <me@terorie.dev>
2022-09-04 18:38:49 +0200

    sbf: move ELFs to fixtures

--- pkg/sbf/loader/relocate_test.go
+++ pkg/sbf/loader/relocate_test.go
@@ -3,9 +3,10 @@ package loader
 import (
 	"testing"
 
+	"github.com/certusone/radiance/pkg/sbf"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSymbolHash_Entrypoint(t *testing.T) {
-	assert.Equal(t, EntrypointHash, SymbolHash("entrypoint"))
+	assert.Equal(t, sbf.EntrypointHash, sbf.SymbolHash("entrypoint"))
 }
