907e65fb6cfb6c48c571b70a8f47348d4764198e
Richard Patel <ripatel@jumptrading.com>
2022-09-18 01:04:48 +0200

    fixtures: make shred sort order natural
    Sorry for the `git pull` jumpscare!

---
+++ cmd/radiance/car/create/create.go
@@ -0,0 +1,23 @@
+package create
+
+import (
+	"github.com/certusone/radiance/pkg/blockstore"
+	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
+)
+
+var Cmd = cobra.Command{
+	Use:   "create <rocksdb...>",
+	Short: "Create CAR from RocksDB",
+	Args:  cobra.MaximumNArgs(1),
+}
+
+func run(_ *cobra.Command, args []string) {
+	rocksDB := args[0]
+
+	db, err := blockstore.OpenReadOnly(rocksDB)
+	if err != nil {
+		klog.Exitf("Failed to open blockstore: %s", err)
+	}
+	defer db.Close()
+}
